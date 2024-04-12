3fe2c29814745ba4306e51ebdfb50895627be47c
thanujsai <sboda@modeln.com>
2024-04-12 17:39:55 +0530

    jsx

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,8 +2,17 @@
 //     return <h1>Hello World!</h1>
 // }
 
+const name = "Thanuj";
+
+const displayMessage = () => {
+    return "message";
+}
 const Hello = () => {//functional component
-    return <h1>Hello World!</h1>
+    return <>
+        <h1>Hello {name}!</h1>
+        <h2>Total is {10+10}</h2>
+        <h1>The message is: {displayMessage()}</h1>
+    </>//this is jsx, curly braces to use variables
 }
 
 export default Hello;//default means we can import this component and gve any name
---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+//class component
+class Message extends Component{//message class is a react component now
+    render() {
+        return <h1>This is a class component</h1>//this is jsx i.e. javascript xml, we can write javascript here
+    }
+}
+
+export default Message;
