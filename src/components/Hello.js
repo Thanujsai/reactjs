a96243f07fb6eb6bd787fdf2411520a2b24108b3
thanujsai <sboda@modeln.com>
2024-04-12 15:50:22 +0530

    first commit

---
+++ src/components/Hello.js
@@ -0,0 +1,9 @@
+// function Hello(){
+//     return <h1>Hello World!</h1>
+// }
+
+const Hello = () => {//functional component
+    return <h1>Hello World!</h1>
+}
+
+export default Hello;//default means we can import this component and gve any name
