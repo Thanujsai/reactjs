89a920c8e81ebbcd4ada6ad0eb41f84a331e827e
Thanujsai <thanuj.sai14301@gmail.com>
2024-04-12 19:18:57 +0530

    props is immutable

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,8 @@
 function Profile(props){
     console.log('props')
     console.log(props)//{name: 'Thanuj'}
+    console.log(props.name)
+    //props.name = "hey";//problem: cannot assign to read only properties, props are immutable, therefore it's not dynamic => we use state
     console.log('Name :' + props.name+' '+props.lastName)
     return (
     <h1>Name: {props.name} {props.lastName} {props.children}</h1>//can pass dynamic data now
