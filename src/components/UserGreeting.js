a5966f368cb70e5ad20cefa1bbae559bf260b0fc
Thanujsai <thanuj.sai14301@gmail.com>
2024-04-28 23:13:41 +0530

    conditional rendering

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -11,12 +11,45 @@ class UserGreeting extends Component {
       }
     }const
   render() {
-    return (
-        <div>
-            <div>Welcome Thanuj</div>
-            <div>Welcome Guest</div>
-      </div>
+    //adding if-else within jsx is not valid
+    // if(this.state.isLoggedIn){
+    //   return(
+    //     <div>Welcome Thanuj</div>
+    //   )
+    // }
+    // else{
+    //   return(
+    //     <div>Welcome Guest</div>
+    //   )
+    // }
+
+    //element variable method
+    // let message;//stores element to be rendered
+    // if(this.state.isLoggedIn){
+    //   message = <div>Welcome Thanuj</div>
+    // }
+    // else{
+    //   message = <div>Welcome Guest</div>
+    // }
+
+    // return <div>{message}</div>
+
+    //ternary conditinal operator method
+    // return(
+    //   this.state.isLoggedIn?<div>Welcome Thanuj</div>:<div>Welcome Guest</div>
+    // )
+
+    //short circuit operator method, when u want to render something or nothing
+    return(
+      this.state.isLoggedIn && <div>Welcome Thanuj</div>//is islogged in is true div tag is displayed
     )
+
+    // return (
+    //     <div>
+    //         <div>Welcome Thanuj</div>
+    //         <div>Welcome Guest</div>
+    //   </div>
+    // )
   }
 }
 
